e23712da8bd620abb526abf376b3192b769899d0
Olivier <ducrouxolivier@gmail.com>
2025-05-09 21:28:01 +0200

    add production.sql

---
+++ db/production.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS pull_api_production;
+CREATE DATABASE IF NOT EXISTS pull_api_production_cache;
+CREATE DATABASE IF NOT EXISTS pull_api_production_queue;
+CREATE DATABASE IF NOT EXISTS pull_api_production_cable;
